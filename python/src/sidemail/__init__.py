51e96237834fd995ed833a9d2ca2354a433b21d4
Patrik Krupar <patrik.krupar@gmail.com>
2025-11-21 15:37:29 +0100

    no longer monorepo

--- python/src/sidemail/__init__.py
+++
@@ -1,4 +0,0 @@
-from .client import Sidemail, SidemailError, SidemailAuthError, SidemailAPIError
-
-__all__ = ["Sidemail", "SidemailError", "SidemailAuthError", "SidemailAPIError"]
-__version__ = "0.1.0"
